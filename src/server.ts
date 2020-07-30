7fb55533b26127ed15f8108cb82a504da1bf329b
Aitor <zheladev@gmail.com>
2020-07-30 22:15:19 +0200

    Middlewares

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,32 @@
 import * as express from 'express';
+import * as bodyParser from 'body-parser';
+
+//middleware type definition
+type Middleware = (req: express.Request, res: express.Response, next: Function) => void;
+
+//example middleware
+const loggerMiddleware: Middleware = (req: express.Request, res: express.Response, next: Function) => {
+    console.log(`${req.method} ${req.path}`);
+    next();
+}
 
 const app = express();
 
+//adding middleware to express
+app.use(loggerMiddleware); 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+
+
+
 app.get('/', (req, res) => {
     res.send('Hello World');
+});
+
+app.post('/', (req, res) => {
+    console.log(req);
+    res.send(req.body);
 });
 
 app.listen(3000);
