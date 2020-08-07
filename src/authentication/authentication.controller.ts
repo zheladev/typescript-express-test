701e3eefce813054762a81a5ebf30d06e58732f6
Aitor <zheladev@gmail.com>
2020-08-07 02:29:54 +0200

    Add JWT authentication

--- src/authentication/authentication.controller.ts
+++ src/authentication/authentication.controller.ts
@@ -7,6 +7,11 @@ import CreateUserDto from "users/user.dto";
 import userModel from "users/user.model";
 import LogInDto from "./logIn.dto";
 import WrongCredentialsException from "exceptions/WrongCredentialsException";
+import TokenData from "interfaces/tokenData.interface";
+import User from "users/user.interface";
+import DataStoredInToken from "interfaces/dataStoredInToken.interface";
+import { IsJWT } from "class-validator";
+import { sign } from "jsonwebtoken";
 
 class AuthenticationController implements Controller {
     public path: string = '/auth';
@@ -43,18 +48,35 @@ class AuthenticationController implements Controller {
         const logInData: LogInDto = request.body;
         const user = await this.user.findOne({ email: logInData.email });
         if (user) {
-            const isPasswordMatching = await bcrypt.compare(logInData.password, user.password);
-            if (isPasswordMatching) {
-                user.password = undefined;
-                response.send(user);
-            } else {
-                next(new WrongCredentialsException());
-            }
-        } else {
+          const isPasswordMatching = await bcrypt.compare(logInData.password, user.password);
+          if (isPasswordMatching) {
+            user.password = undefined;
+            const tokenData = this.createToken(user);
+            response.setHeader('Set-Cookie', [this.createCookie(tokenData)]);
+            response.send(user);
+          } else {
             next(new WrongCredentialsException());
+          }
+        } else {
+          next(new WrongCredentialsException());
         }
     };
 
+    private createToken(user: User) : TokenData {
+        const expiresIn = 60*60;
+        const secret = '21d1209u21d0921d12'; //set in ENV
+        const dataStoredInToken: DataStoredInToken = {
+            _id: user._id
+        };
+        return {
+            expiresIn,
+            token: sign(dataStoredInToken, secret, {expiresIn: expiresIn})
+        };
+    }
+
+    private createCookie(tokenData: TokenData) {
+        return `Authorization=${tokenData.token}; HttpOnly; Max-Age=${tokenData.expiresIn}`;
+      }
 
 }
 
